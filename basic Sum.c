b9d9b15b3ec162cac822ae71edd8a478087c3d96
MDSHAHRIARKHANSINHA <mdshahriarkhansinha@gmail.com>
2024-08-22 12:09:21 +0600

    Add files via upload

---
+++ basic Sum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main () {
+    int a,b,sum;
+    printf("Please Inter the number Input");
+    scanf("%d %d",&a,&b);
+    sum = a+b;
+    printf("Sum of two numbers is %d",sum);
+    return 0;
+
+}
